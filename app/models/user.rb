611c66b63cfa7ff2b509c6c914c6fc1b8d7966ce
pountzas <nikos@pountzas.gr>
2021-10-12 21:29:58 +0300

    correct use model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,9 @@
-require 'rails_helper'
+class User < ApplicationRecord
+  # encrypt password
+  has_secure_password
 
-# Test suite for User model
-RSpec.describe User, type: :model do
-  # Association test
-  # ensure User model has a 1:m relationship with the Todo model
-  it { should have_many(:todos) }
-  # Validation tests
-  # ensure name, email and password_digest are present before save
-  it { should validate_presence_of(:name) }
-  it { should validate_presence_of(:email) }
-  it { should validate_presence_of(:password_digest) }
+  # Model associations
+  has_many :todos, foreign_key: :created_by
+  # Validations
+  validates_presence_of :name, :email, :password_digest
 end
